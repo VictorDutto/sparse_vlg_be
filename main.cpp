27b93b0195c1a3563f76c83ee5770fce3c56d007
Victor Dutto <victor.dutto@epita.fr>
2021-06-02 17:07:29 +0200

    constructing graph

--- main.cpp
+++ main.cpp
@@ -3,6 +3,10 @@
 int main()
 {
    igraph_real_t diameter;
-   igraph_t graph; 
+   FILE* f = fopen("amazon0302.txt", "R");
+   igraph_t *graph;
+   igraph_bool_t b = false;
+   igraph_read_graph_edgelist(graph, f, 262110, b);
+   fclose(f);
    return 0;
 }
