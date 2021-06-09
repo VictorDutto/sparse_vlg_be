546d86b557a302eca389804e4b9aa79de897cd87
Victor Dutto <victor.dutto@epita.fr>
2021-06-09 16:48:47 +0200

    can read graph

--- main.cpp
+++ main.cpp
@@ -1,12 +1,19 @@
 #include <igraph.h>
+#include <stdio.h>
 
 int main()
 {
    igraph_real_t diameter;
-   FILE* f = fopen("amazon0302.txt", "R");
-   igraph_t *graph;
+   FILE* f = fopen("amazon0302.txt", "r");
+   if (!f)
+   {
+      return 1;
+   }
+   int size = 1024;
+   igraph_t graph;
    igraph_bool_t b = false;
-   igraph_read_graph_edgelist(graph, f, 262110, b);
+   int error = igraph_read_graph_edgelist(&graph, f, 0, b);
+   printf("There is %d edges in the graph\n", igraph_ecount(&graph));
    fclose(f);
    return 0;
 }
