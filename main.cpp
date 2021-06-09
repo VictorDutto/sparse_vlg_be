fd2fb11196529528ec623bdc820ea81019535035
Victor Dutto <victor.dutto@epita.fr>
2021-06-09 17:03:41 +0200

    fixed path to file

--- main.cpp
+++ main.cpp
@@ -1,19 +1,21 @@
 #include <igraph.h>
-#include <stdio.h>
+#include <cstdio>
+#include <cstdlib>
 
 int main()
 {
    igraph_real_t diameter;
-   FILE* f = fopen("amazon0302.txt", "r");
+   FILE* f = std::fopen("../amazon0302.txt", "r");
    if (!f)
    {
-      return 1;
+      std::perror("Open failed\n");
+      return EXIT_FAILURE;
    }
    int size = 1024;
    igraph_t graph;
    igraph_bool_t b = false;
    int error = igraph_read_graph_edgelist(&graph, f, 0, b);
    printf("There is %d edges in the graph\n", igraph_ecount(&graph));
-   fclose(f);
+   std::fclose(f);
    return 0;
 }
