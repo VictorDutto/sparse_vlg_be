ea3799e4cf2aafdaee4246b469aec5c2e6dae332
Quentin Le Helloco <quentinlehelloco@Quentins-MacBook-Air.local>
2021-07-14 20:59:15 +0200

    Add file as a parameter

--- main.cpp
+++ main.cpp
@@ -8,30 +8,34 @@
 
 int main(int argc, char *const argv[])
 {
+   printf_wrapper("Bounding Eccentricities algorithm\n");
+   int opt, option_index;
+   int strategy = 0;
+   std::string graph_name = "p2p-Gnutella04.txt";
 
-   int option_index = 0;
    static struct option long_options[] = {
       {"basic",    0, NULL,  0 },
       {"density",  0, NULL,  1 },
-      {"degree",   0, NULL,  2 }
+      {"degree",   0, NULL,  2 },
+      {"file",     1, NULL,  3 }
    };
 
-   // Get parameter for strategies
-   int opt = getopt_long(argc, argv, "", long_options, &option_index);
-   
-   if (opt == -1)
+   // Get parameter for strategies and assign file
+   while ((opt = getopt_long(argc, argv, "", long_options, &option_index)) != -1)
    {
-      fprintf(stderr, "Usage: %s [-t nsecs] [-n] name\n", argv[0]);
-      exit(EXIT_FAILURE);
+      if (option_index == 3)
+         graph_name.assign(optarg);
+      else
+         strategy = option_index;
    }
+   printf_wrapper("%d\n", strategy);
 
    igraph_real_t diameter;
    //graph format must be separated with whitespaces
    //this project is not meant to be a wrapper around igraph_read_graph_edgelist
-   std::string graph_name("p2p-Gnutella04.txt");
    std::string arg("../graph/");
    arg = arg  + graph_name;
-   printf_wrapper("%s\n", arg.c_str());
+   printf_wrapper("Using file: %s\n\n", arg.c_str());
    FILE* f = std::fopen(arg.c_str(), "r");
    if (!f)
    {
@@ -48,7 +52,7 @@ int main(int argc, char *const argv[])
 
    // Get greatest connected components.
    auto gcc = init_gcc(&graph);
-   auto ecc_vect = calculate_eccentricity(gcc, option_index);
+   auto ecc_vect = calculate_eccentricity(gcc, strategy);
    size_t lgt = ecc_vect.size();
    for (size_t i = 0; i < lgt; i = 1 + i + 0.1 * lgt)
       printf_wrapper("This vector contains an eccentricity of value: %i\n", ecc_vect[i]);
